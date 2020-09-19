88877a9d043379d450a55f0c41a29c4275f0c80a
Snorlax <5159138-EmeraldSnorlax@users.noreply.gitlab.com>
2020-09-19 15:44:00 +0000

    fix grabbing port from process.env.PORT

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 const morgan = require('morgan')
 
-const port = 3000 || process.env.PORT
+const port = process.env.PORT || 3000
 
 const game = require('./routes/game')
 
