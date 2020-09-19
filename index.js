12a5105c37328db1dd18fd291a39eff0f23090bf
EmeraldSnorlax <5159138-EmeraldSnorlax@users.noreply.gitlab.com>
2020-09-19 12:45:19 +0100

    use morgan middleware

--- index.js
+++ index.js
@@ -1,11 +1,18 @@
 const express = require('express')
 const app = express()
+const morgan = require('morgan')
+
 const port = 3000 || process.env.PORT
 
+const game = require('./routes/game')
+
+app.use(morgan('dev'))
+app.use('/game', game)
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
+
 
 app.listen(port, () => {
   console.log(`Listening on ${port}`)
